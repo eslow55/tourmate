ec0fc4472043d0d2c03667e28e355acf465c122f
SrSanm <asanmartinvalencia@gmail.com>
2026-05-08 11:13:46 -0500

    cambios en el vite

--- TourMate/vite.config.js
+++ TourMate/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Asegura que los archivos se busquen de forma relativa
+  base: "/", // Asegura que los archivos se busquen de forma relativa
   build: {
     outDir: "dist", // Confirma que la carpeta de salida sea dist
   },
